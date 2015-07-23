ec030f5aece24dd1b46b255e333aaf6e44bde71a
Nelson Martell <nelson6e65-dev@yahoo.es>
2015-07-23 09:07:07 -0430

    Updated 'es' translations

--- es/migration_lang.php
+++ es/migration_lang.php
@@ -1,12 +1,12 @@
 <?php
 
-$lang['migration_none_found']          = "No migrations were found.";
-$lang['migration_not_found']           = "This migration could not be found.";
-$lang['migration_multiple_version']    = "This are multiple migrations with the same version number: %d.";
-$lang['migration_class_doesnt_exist']  = "The migration class \"%s\" could not be found.";
-$lang['migration_missing_up_method']   = "The migration class \"%s\" is missing an 'up' method.";
-$lang['migration_missing_down_method'] = "The migration class \"%s\" is missing an 'down' method.";
-$lang['migration_invalid_filename']    = "Migration \"%s\" has an invalid filename.";
+$lang['migration_none_found']          = "No se encontraron migraciones.";
+$lang['migration_not_found']           = "Las migraciones no pudieron ser encontradas.";
+$lang['migration_multiple_version']    = "Hay múltiples migraciones con el mismo número de versión; %d.";
+$lang['migration_class_doesnt_exist']  = "La clase de migración \"%s\" no pudo ser encontrada.";
+$lang['migration_missing_up_method']   = "A la clase de migración \"%s\" le falta el método 'up'.";
+$lang['migration_missing_down_method'] = "A la clase de migración \"%s\" le falta el método 'down'.";
+$lang['migration_invalid_filename']    = "La migración \"%s\" tiene un nombre de archivo inválido.";
 
 
 /*
--- es/profiler_lang.php
+++ es/profiler_lang.php
@@ -1,25 +1,25 @@
 <?php
 
-$lang['profiler_database']        = 'DATABASE';
-$lang['profiler_controller_info'] = 'CLASS/METHOD';
+$lang['profiler_database']        = 'BASE DE DATOS';
+$lang['profiler_controller_info'] = 'CLASE/MÉTODO';
 $lang['profiler_benchmarks']      = 'BENCHMARKS';
-$lang['profiler_queries']         = 'QUERIES';
-$lang['profiler_get_data']        = 'GET DATA';
-$lang['profiler_post_data']       = 'POST DATA';
-$lang['profiler_uri_string']      = 'URI STRING';
-$lang['profiler_memory_usage']    = 'MEMORY USAGE';
-$lang['profiler_config']          = 'CONFIG VARIABLES';
-$lang['profiler_session_data']    = 'SESSION DATA';
-$lang['profiler_headers']         = 'HTTP HEADERS';
-$lang['profiler_no_db']           = 'Database driver is not currently loaded';
-$lang['profiler_no_queries']      = 'No queries were run';
-$lang['profiler_no_post']         = 'No POST data exists';
-$lang['profiler_no_get']          = 'No GET data exists';
-$lang['profiler_no_uri']          = 'No URI data exists';
-$lang['profiler_no_memory']       = 'Memory Usage Unavailable';
-$lang['profiler_no_profiles']     = 'No Profile data - all Profiler sections have been disabled.';
-$lang['profiler_section_hide']    = 'Hide';
-$lang['profiler_section_show']    = 'Show';
+$lang['profiler_queries']         = 'CONSULTAS';
+$lang['profiler_get_data']        = 'DATOS GET';
+$lang['profiler_post_data']       = 'DATOS POST';
+$lang['profiler_uri_string']      = 'CADENA URI';
+$lang['profiler_memory_usage']    = 'USO DE MEMORIA';
+$lang['profiler_config']          = 'VARIABLES DE CONFIGURACIÓN';
+$lang['profiler_session_data']    = 'DATOS DE SESIÓN';
+$lang['profiler_headers']         = 'CABECERAS HTTP';
+$lang['profiler_no_db']           = 'El controlador de base de datos no está cargado actualmente';
+$lang['profiler_no_queries']      = 'No se encontraron consultas';
+$lang['profiler_no_post']         = 'No existen datos POST';
+$lang['profiler_no_get']          = 'No existen datos GET';
+$lang['profiler_no_uri']          = 'No existen datos URI';
+$lang['profiler_no_memory']       = 'Uso de memoria no disponible';
+$lang['profiler_no_profiles']     = 'Sin datos de perfilamiento - todas las sesiones del Perfilador han sido deshabilitadas.';
+$lang['profiler_section_hide']    = 'Ocultar';
+$lang['profiler_section_show']    = 'Mostrar';
 
 /*
     End of file profiler_lang.php */
